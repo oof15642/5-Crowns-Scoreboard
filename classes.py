5f143d74db687b36554840245c88695c4fb6a974
oof15642 <59772241+oof15642@users.noreply.github.com>
2023-12-17 17:04:12 -0700

    wow okay
    soo this probably wont work so either research more ideas or find a better way to do this.

--- classes.py
+++ classes.py
@@ -1,17 +1,44 @@
 import tkinter as tk
 
+playerCount = 0
+#playerDict =
+
 class Entry_Box:
 
-    def __init__(self, main, bg, font, fg, label, column, row):
+    def __init__(self, main, bg, fg, font, labelFont, label, buttonText, column, row):
         self.main = main
         self.bg = bg
-        self.font = font
         self.fg = fg
+        self.font = font
+        self.labelFont = labelFont
         self.label = label
+        self.buttonText = buttonText
         self.column = column
         self.row = row
 
     def make_box(self):
-        self.entry = tk.Entry(self.main, bg="lightblue", fg="black", font=self.font)
+        self.entryLabel = tk.Label(self.main, font=self.labelFont, text=self.label)
+        self.entryLabel.grid(column=self.column, row=self.row, sticky="w")
 
-        self.entry.grid(column=self.column, row=self.row)
+        self.entry = tk.Entry(self.main, bg=self.bg, fg=self.fg, font=self.font)
+        self.entry.grid(column=self.column, row=self.row+1)
+
+        self.button = tk.Button(self.main, bg=self.bg, command=self.return_text, fg=self.fg, text=self.buttonText)
+        self.button.grid(column=self.column+1, row=self.row+1)
+
+    def return_text(self):
+        global playerCount
+        global playerDict
+
+        playerCount = self.entry.get()
+        print(playerCount)
+
+        self.entryLabel.grid_remove()
+        self.entry.grid_remove()
+        self.button.grid_remove()
+
+        for player in playerCount:
+            #self.playerEntryLabel = tk.Label(self.main, font=)
+
+
+            playerDict = {player: num*num for num in range(1, 11)}
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#########   PLEASE LOOK AT PREVIOUS COMMIT SUMMARRY   #########
+
 import classes
 import tkinter as tk
 
@@ -5,9 +7,10 @@ main = tk.Tk()
 main.title("First Tkinter Window")
 main.geometry("600x400")
 
-playerCountEntry = classes.Entry_Box(main, "#00FFFF", "#00FFFF", ("Courier New",14), "#000000", "heyy", 0, 0)
-playerCountEntry.make_box()
-
-playerCountEntry2 = classes.Entry_Box(main, "#00FFFF", "#00FFFF", ("Bahnschrift",14), "#000000", "heyy", 0, 1)
+playerCountEntry2 = classes.Entry_Box(main, "lightblue", "black", ("Courier New CYR", 14), ("Courier New CYR", 16), "How many players are there?", "Submit", 0, 1)
 playerCountEntry2.make_box()
+
+if classes.playerCount > 0:
+    print("hey")
+
 main.mainloop()
